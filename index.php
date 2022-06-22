108f8c33be3ef8363444e00a31f1517aa53b4e71
sgarcia-dev <s.garcia@codeur.online>
2022-06-22 10:54:57 +0200

    delete svg

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include 'include_header.php'; ?>
 
-<img src="media/logo_sopasy.svg" width="55px" alt="logo sopasy">
+<img src="media/logo_sopasy.png" alt="logo sopasy">
 
 <?php include 'include_footer.php'; ?>
